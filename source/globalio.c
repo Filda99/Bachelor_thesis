536b3b760f64aaf8ec15cfebdcb1e24429a82fe3
Filda99 <52467691+Filda99@users.noreply.github.com>
2023-05-14 21:26:58 +0200

    Fix lost lines

--- source/globalio.c
+++ source/globalio.c
@@ -19,6 +19,9 @@
 //* Number of wheel rotations
 unsigned int HalfWheelRotations = 0;
 
+//* Which lines are detected at the moment
+uint8_t LineDetected = LineNone;
+
 // Stop car, because some error has occured
 // When IsCmdToStopCar is true, no more action will be taken
 // Last thing is to save map
@@ -27,6 +30,11 @@ bool IsCmdToStopCar = false;
 // In initial initialization there is need to rotate wheel to starting position and hold this
 // position until car starts. With this we can be sure, that distance measurement is correct.
 bool InitStop = true;
+
+// Left sensor color value
+unsigned int LeftSensorValue = 0;
+unsigned int CenterSensorValue = 0;
+unsigned int RightSensorValue = 0;
 
 //! Current position in speed array.
 //! 0 = max backwards
